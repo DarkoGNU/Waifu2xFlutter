478babda2deec95e02c2e3580c7bb3c55b6bb182
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-03-01 23:08:04 +0100

    Adaptive themes - preparations

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:waifu2x_flutter/themes.dart';
 
 import 'Pickers/tab_selector.dart';
 
-void main() {
-  runApp(const WaifuApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+
+  return runApp(WaifuApp(savedThemeMode: savedThemeMode!));
 }
 
 class WaifuApp extends StatelessWidget {
-  const WaifuApp({Key? key}) : super(key: key);
+  const WaifuApp({Key? key, required this.savedThemeMode}) : super(key: key);
+
+  final AdaptiveThemeMode savedThemeMode;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Waifu2xFlutter',
-      home: const WaifuHome(title: 'Upscale an image'),
-      theme: lightTheme(context),
+    return AdaptiveTheme(
+      light: lightTheme(context),
+      dark: lightTheme(context), // TODO: implement dark theme
+      initial: savedThemeMode,
+      builder: (lightTheme, darkTheme) => MaterialApp(
+        title: 'Waifu2xFlutter',
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        home: const WaifuHome(title: 'Upscale an image'),
+      ),
     );
   }
 }
